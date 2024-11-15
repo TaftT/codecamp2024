67b196dc7ea0f9e8d6e94cecf8410479006960b7
TaftT <thisTaft@gmail.com>
2024-11-15 16:43:17 -0700

    starting to mpleent vue

--- client/code.js
+++ client/code.js
@@ -2,9 +2,30 @@ var AppName = new Vue({
     el: '#AppName',
     data: {
         test: 'Hello test',
+        competitions:[],
+        page:"index",
+        competition:null
     },
     methods: {
         myMethod: function () {
+        },
+        runRoute: function() {
+            console.log("here")
+            fetch("https://app-ia6miajuua-uc.a.run.app/competitions/all").then( (response)=>{
+                response.json().then( (data) => {
+                    console.log("important",data.data);
+                    this.competitions=data.data;
+                    console.log(this.competitions)
+            
+                });
+            })
+        },
+        goToComp: function (comp, pageChange) {
+            this.page='competition';
+            this.competition =comp;
+        },
+        changePage: function (vari) {
+            this.page=vari;
         }
     },
     filters: {
@@ -15,5 +36,9 @@ var AppName = new Vue({
     },
     created: function () {
         // when app loads run this code
+        this.runRoute()
     }
 })
+
+ 
+//
